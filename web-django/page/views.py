a1e4dbc7ffe24e0f411a4eec869640366385eba8
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-06-23 21:53:42 +0200

    version-final

--- web-django/page/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import SensorsData
-
-# Create your views here.
-
-def index(request):
-    temp = SensorsData.objects.all()
-    return render(request, 'index.html', {'temp': temp})
